b65fb8579942de006c66bf9a31d6a265f07a1f7b
DrMOwOstache <114975717+DrMOwOstache@users.noreply.github.com>
2023-05-11 08:48:31 +0300

    correct

--- lab/lab.cpp
+++ lab/lab.cpp
@@ -1,125 +1,155 @@
 #include "lab.h"
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QGroupBox>
-#include <QTextEdit>
-#include <QRadioButton>
-#include <QLabel>
-#include <QLineEdit>
-#include <QListWidget>
-#include <QFormLayout>
-#include <QRect>
+
 
 lab::lab(QWidget *parent)
     : QMainWindow(parent)
 {
-    int standartHeight = 5;
-    int standartIndent = 0;
-
-    QGroupBox* musicTab = new QGroupBox(tr("music"));
-
-    QHBoxLayout* mainMusic = new QHBoxLayout;
-    QVBoxLayout* firstColumn = new QVBoxLayout;
-
-
-    /*QFormLayout* subTitle = new QFormLayout;
-    QFormLayout* subArtist = new QFormLayout;
-    QFormLayout* subDuration = new QFormLayout;
-    QFormLayout* subLink = new QFormLayout;*/
-    QFormLayout* subLabels = new QFormLayout;
-    QHBoxLayout* subSongs = new QHBoxLayout;
-
-
-    //++++++++++++++++//
-    QLabel* Songs = new QLabel;
-    Songs->setText("All songs");
-    Songs->setIndent(0);
-    Songs->setMargin(standartHeight);
-    subSongs->addWidget(Songs);
-
-    firstColumn->addItem(subSongs);
-
-    //++++++++++++++++//
-    QListWidget* melody = new QListWidget;
-    //melody->setFixedHeight(standartHeight);
-
-    firstColumn->addWidget(melody);
-
-    //++++++++++++++++//
-    QLabel* Title = new QLabel;
-    Title->setText("Title:");
-    Title->setIndent(standartIndent);
-    Title->setMargin(standartHeight);
-    //subTitle->addWidget(Title);
-
-    QLineEdit* lineTitle = new QLineEdit;
-    //subTitle->addWidget(lineTitle);
-    subLabels->addRow(Title, lineTitle);
-
-
-    //++++++++++++++++//
-    QLabel* Artist = new QLabel;
-    Artist->setText("Artist:");
-    Artist->setIndent(standartIndent);
-    Artist->setScaledContents(true);
-    Artist->setMargin(standartHeight);
-    //subArtist->addWidget(Artist);
-
-    QLineEdit* lineArtist = new QLineEdit;
-
-    subLabels->addRow(Artist, lineArtist);
-
-    //subArtist->addWidget(lineArtist);
-    //firstColumn->addItem(subArtist);
-
-    //++++++++++++++++//
-    QLabel* Duration = new QLabel;
-    Duration->setText("Duration:");
-    Duration->setIndent(standartIndent);
-    Duration->setMargin(standartHeight);
-    //subDuration->addWidget(Duration);
-
-    QLineEdit* lineDuration = new QLineEdit;
-
-    subLabels->addRow(Duration, lineDuration);
-
-    //subDuration->addWidget(lineDuration);
-    //firstColumn->addItem(subDuration);
-
-    //++++++++++++++++//
-    QLabel* Link = new QLabel;
-    Link->setText("Link:");
-    Link->setIndent(standartIndent);
-    Link->setMargin(standartHeight);
-    //subLink->addWidget(Link);
-
-    QLineEdit* lineLink = new QLineEdit;
-
-    subLabels->addRow(Link, lineLink);
-
-    //subLink->addWidget(lineLink);
-    //firstColumn->addItem(subLink);
-
-    //++++++++++++++++//
-    
-   
-
-
-    //++++++++++++++++//
-    //----------------//
-
-
-    
-
-    //----------------//
-    firstColumn->addItem(subLabels);
-    mainMusic->addItem(firstColumn);
-    musicTab->setLayout(mainMusic);
-    //----------------//
-    musicTab->show();
-
-    ui.setupUi(this);
+    this->setupUI();
+    //ui.setupUi(this);
 }
 
 lab::~lab()
 {}
+
+void lab::setupUI()
+{
+    int standartHeight = 5;
+    int standartIndent = 0;
+    //================//
+
+    centralWidget = new QWidget;
+
+    //================//
+
+    mainMusic = new QHBoxLayout;
+    firstColumn = new QVBoxLayout;
+
+    //================//
+
+    subLabels = new QFormLayout;
+    subSongs = new QHBoxLayout;
+
+    //================//
+    songOptions = new QGridLayout;
+
+    songOptions->setSpacing(5);
+    //================//
+    middleButton = new QHBoxLayout;
+
+    //================//
+    rightColumn = new QVBoxLayout;
+
+    //================//
+    playlistOptions = new QGridLayout;
+
+    //================//
+    //++++++++++++++++//
+    Songs = new QLabel("All songs");
+    melody = new QListWidget;
+
+    subSongs->addWidget(Songs);
+
+    firstColumn->addItem(subSongs);
+    firstColumn->addWidget(melody);
+
+    //++++++++++++++++//
+    Title = new QLabel("Title:");
+    lineTitle = new QLineEdit;
+
+    Title->setMargin(standartHeight);
+
+    subLabels->addRow(Title, lineTitle);
+
+    //++++++++++++++++//
+    Artist = new QLabel("Artist:");
+    lineArtist = new QLineEdit;
+
+    Artist->setMargin(standartHeight);
+
+    subLabels->addRow(Artist, lineArtist);
+
+    //++++++++++++++++//
+    Duration = new QLabel("Duration:");
+    lineDuration = new QLineEdit;
+
+    Duration->setMargin(standartHeight);
+
+    subLabels->addRow(Duration, lineDuration);
+
+    //++++++++++++++++//
+    Link = new QLabel("Link:");
+    lineLink = new QLineEdit;
+
+    Link->setMargin(standartHeight);
+
+    subLabels->addRow(Link, lineLink);
+
+    //++++++++++++++++//
+
+    addB = new QPushButton("Add");
+
+    songOptions->addWidget(addB,0,0);
+
+    //++++++++++++++++//
+
+    delB = new QPushButton("Delete");
+
+    songOptions->addWidget(delB,0,1);
+
+    //++++++++++++++++//
+
+    updB = new QPushButton("Update");
+
+    songOptions->addWidget(updB,0,2);
+
+    //++++++++++++++++//
+
+    fltB = new QPushButton("Filter");
+
+    songOptions->addWidget(fltB,1,1);
+
+    //++++++++++++++++//
+    
+    transferB = new QPushButton(">");
+    
+    middleButton->addWidget(transferB);
+
+    //++++++++++++++++//
+
+    playlist = new QLabel("Playlist");
+
+    rightColumn->addWidget(playlist);
+
+    //++++++++++++++++//
+
+    playlist_List = new QListWidget;
+
+    rightColumn->addWidget(playlist_List);
+    //++++++++++++++++//
+
+    playB = new QPushButton("Play");
+    nextB = new QPushButton("Next");
+
+    playlistOptions->addWidget(playB, 0, 0);
+    playlistOptions->addWidget(nextB, 0, 1);
+
+    //++++++++++++++++//
+    //----------------//
+
+    firstColumn->addItem(subLabels);
+    firstColumn->addItem(songOptions);
+
+    //----------------//
+
+    rightColumn->addItem(playlistOptions);
+
+    //----------------//
+    mainMusic->addItem(firstColumn);
+    mainMusic->addItem(middleButton);
+    mainMusic->addItem(rightColumn);
+    centralWidget->setLayout(mainMusic);
+    this->setCentralWidget(centralWidget);
+
+    //----------------//
+
+}
--- lab/lab.h
+++ lab/lab.h
@@ -1,6 +1,18 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGroupBox>
+#include <QTextEdit>
+#include <QRadioButton>
+#include <QLabel>
+#include <QLineEdit>
+#include <QListWidget>
+#include <QFormLayout>
+#include <QRect>
+#include <QGridLayout>
+#include <QPushButton>
 #include "ui_lab.h"
 
 class lab : public QMainWindow
@@ -12,5 +24,55 @@ public:
     ~lab();
 
 private:
+    void setupUI();
+
+    //==
+    QWidget* centralWidget;
+    //==
+    QHBoxLayout* mainMusic;
+    QVBoxLayout* firstColumn;
+    //==
+    QFormLayout* subLabels;
+    QHBoxLayout* subSongs;
+    //==
+    QGridLayout* songOptions;
+    //==
+    QHBoxLayout* middleButton;
+    //==
+    QVBoxLayout* rightColumn;
+    //==
+    QGridLayout* playlistOptions;
+    //==
+    //first column
+    //++
+    QLabel* Songs;
+    QListWidget* melody;
+    //++
+    QLabel* Title;
+    QLineEdit* lineTitle;
+    //++
+    QLabel* Artist;
+    QLineEdit* lineArtist;
+    //++
+    QLabel* Duration;
+    QLineEdit* lineDuration;
+    //++
+    QLabel* Link;
+    QLineEdit* lineLink;
+    //++
+    QPushButton* addB;
+    QPushButton* delB;
+    QPushButton* updB;
+    QPushButton* fltB;
+    //++
+    QPushButton* transferB;
+    //++
+    QLabel* playlist;
+    QListWidget* playlist_List;
+    //++
+    QPushButton* playB;
+    QPushButton* nextB;
+    //++
+
     Ui::labClass ui;
 };
